845551db139dd8485d9bfe6d2a155ad613f16beb
Nitesh-Tyagi <tyagipsgn@gmail.com>
2022-05-15 02:15:21 +0530

    Fifth Commit

--- pokemash2/settings.py
+++ pokemash2/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
